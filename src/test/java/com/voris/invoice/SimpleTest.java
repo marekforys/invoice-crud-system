fd0c6814dff6c26c21c34982adae680b4d5bf134
Marek Forys <mforys73@gmail.com>
2025-09-03 13:05:10 +0200

    test: Simple Test added as health check of testing system
    Signed-off-by: Marek Forys <mforys73@gmail.com>

---
+++ src/test/java/com/voris/invoice/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.voris.invoice;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleTest {
+    @Test
+    void simpleTest() {
+        assertTrue(true, "This test should always pass");
+    }
+}
